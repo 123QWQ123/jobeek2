913e09de326b8a7b208c4c0602951d748a48ea6e
Akhmedov Farkhod <a.farkhod@gmail.com>
2022-02-10 16:04:53 +0500

    Добавлены исходные стили верстки

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./styles/styles.scss";
+
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://localhost:8000'; //process.env.API_URL
 
