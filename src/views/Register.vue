e0df010a2d2f464e076ef1f76105c9962a2dca13
Akhmedov Farkhod <a.farkhod@gmail.com>
2022-02-11 10:46:22 +0500

    Установлен новый дизайн для регистрации

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,44 +1,113 @@
 <template>
-  <div v-if="codeConfirmation">
-    <p>Подтвердите код</p>
-    <p>Номер телефона: {{ phone }}</p>
-    <input v-model="code" type="text">
-    <button @click="confirmCode()" type="button">Подтвердить</button>
-    <button @click="back()" type="button">Отмена</button>
-  </div>
-  <div v-else>
-    <input v-model="phone" type="text" placeholder="Телефон">
-    <button @click="sendCode()" type="button">Получить код</button>
-  </div>
+  <main class="main enter-page sign-up" role="main">
+    <div class="enter-page-content">
+      <div class="logo">
+        <img src="../assets/img/jobeek-dark.svg" alt="#" />
+      </div>
+      <form v-if="codeConfirmation" @submit.prevent="confirmCode()" class="enter-form">
+        <h1>Регистрация</h1>
+        <div class="i-wrap">
+          <input v-model="code" type="text" placeholder="Код подтверждения" />
+          <div v-if="errors?.code" class="error">{{ errors.code[0] }}</div>
+        </div>
+        <div class="help-box">
+          <div class="check-block">
+            <div class="checkbox">
+              <input type="checkbox" id="agree" checked disabled />
+              <div class="checkbox-mask">
+                <img src="../assets/img/svg/check.svg" alt="#" />
+              </div>
+            </div>
+            <label for="agree">
+              Согласен с
+              <a href="#">правилами обработки персональных данных</a>
+            </label>
+          </div>
+        </div>
+        <button class="btn button-accent" type="submit">
+          Подтвердить код
+        </button>
+      </form>
+      <form v-else @submit.prevent="sendCode()" class="enter-form">
+        <h1>Регистрация</h1>
+        <div class="i-wrap">
+          <phone-field v-model:phone="phone" />
+          <div v-if="errors?.phone" class="error">{{ errors.phone[0] }}</div>
+        </div>
+        <div class="help-box">
+          <div class="check-block">
+            <div class="checkbox">
+              <input type="checkbox" id="agree" checked disabled />
+              <div class="checkbox-mask">
+                <img src="../assets/img/svg/check.svg" alt="#" />
+              </div>
+            </div>
+            <label for="agree">
+              Согласен с
+              <a href="#">правилами обработки персональных данных</a>
+            </label>
+          </div>
+        </div>
+        <button class="btn button-accent" type="submit">
+          Зарегистрироваться
+        </button>
+      </form>
+      <div class="f-prompt">
+        Уже есть аккаунт? <router-link to="/login">Войдите!</router-link>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import axios from 'axios';
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import PhoneField from "../components/PhoneField";
 
 export default {
+  components: {
+    "phone-field": PhoneField
+  },
+
   setup() {
     const store = useStore();
+    const router = useRouter();
 
     const codeConfirmation = ref(false);
-    const phone = ref('12345678910');
+    const phone = ref('');
     const code = ref('');
+    const errors = ref([]);
+    const confirmationSession = ref('');
 
-    let confirmationSession = '';
+    const unformattedPhone = computed(() => {
+      return phone.value.replace(/\D/g, '');
+    })
 
     const sendCode = () => {
-      codeConfirmation.value = true;
-
-      axios.post('/api/auth/register', { phone: phone.value }).then(({ data }) => {
-        confirmationSession = data.data.session;
-      });
+      axios
+        .post('/api/auth/register', { phone: unformattedPhone.value })
+        .then(({ data }) => {
+          codeConfirmation.value = true;
+          confirmationSession.value = data.data.session;
+        })
+        .catch(({response}) => errors.value = response.data.errors);
     }
 
     const confirmCode = () => {
-      axios.post('/api/auth/register/confirm', { phone: phone.value, session: confirmationSession, code: code.value }).then(() => {
-        store.dispatch('login', { phone: phone.value, password: code.value });
-      });
+      axios
+        .post('/api/auth/register/confirm', {
+          phone: unformattedPhone.value,
+          session: confirmationSession.value,
+          code: code.value
+        })
+        .then(() => {
+          store.dispatch('login', { phone: unformattedPhone.value, password: code.value })
+            .then(() => router.push('/'))
+            .catch(response => errors.value = response.errors);
+        })
+        .catch(({response}) => errors.value = response.data.errors);
     }
 
     const back = () => {
@@ -50,6 +119,7 @@ export default {
       codeConfirmation,
       phone,
       code,
+      errors,
       sendCode,
       confirmCode,
       back,
