9ce6c4e7c6eeeeadc2f0d7d6489bca7543ae0c5d
orzumurodparmonov <orzumurodparmonov@gmail.com>
2023-06-22 18:12:09 +0500

    clean up

--- components/UI/SelectWithSearch.vue
+++ components/UI/SelectWithSearch.vue
@@ -32,13 +32,11 @@ const props = defineProps({
 
 const isFirst = ref(false);
 const isOpen = ref(false);
-console.log(props.options);
 const options = ref(props.options);
 const placeholder = computed(() => props.placeholder);
 
 watch(() => props.options, (newOptions) => {
   options.value = newOptions;
-  console.log(props.modelValue);
   selectedOption.value = options.value.find(item => String(item.value) === String(props.modelValue));
 });
 
@@ -53,7 +51,6 @@ onMounted(() => {
 const labelText = computed(() => {
 
     if (!isOpen.value){
-        console.log(selectedOption.value);
         if (selectedOption.value){
             return selectedOption.value.name;
         }else{
