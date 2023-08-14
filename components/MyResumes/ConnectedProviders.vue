1617daa03e507cb2402cc0c71c0e4749b3554068
orzumurodparmonov <orzumurodparmonov@gmail.com>
2023-08-14 15:42:27 +0500

    connected providers issues fixed

--- components/MyResumes/ConnectedProviders.vue
+++ components/MyResumes/ConnectedProviders.vue
@@ -1,30 +1,48 @@
 <template>
-    <div class="wrapper wrapper-1290" v-if="!isAllConnected">
-        <h1 class="lk-page-title">Cервисы</h1>
-        {{providersList}}
+    <div>
+      <ClientOnly>
+      <div class="wrapper wrapper-1290" v-if="!isAllConnected">
+        <h1 class="lk-page-title">Cервисы1</h1>
         <div class="modal-content p-2 m-0 border-0" style="min-width: 10rem;">
-            <div class="list-of-providers">
-                <a @click="openProviderAuthUrl(item.url)" v-for="item in providersList" class="provider-item">
+          <div class="list-of-providers">
+            <a @click="openProviderAuthUrl(providers.hh.url)"  class="provider-item">
                   <span class="provider-label success">
-                    <svg v-if="item.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
+                    <svg v-if="providers.hh.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
                          viewBox="0 0 16 16">
                       <path
-                              d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                          d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>
                     <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f14646" class="bi bi-x"
                          viewBox="0 0 16 16">
                       <path
-                              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                     </svg>
                   </span>
-                    <img :src="item.icon" />
-                </a>
-            </div>
+              <img :src="'https://tech.hh.ru/api/logos/min-hh-red.png'" />
+            </a>
+            <a @click="openProviderAuthUrl(providers.superjob.url)"  class="provider-item">
+                  <span class="provider-label success">
+                    <svg v-if="providers.superjob.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
+                         viewBox="0 0 16 16">
+                      <path
+                          d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f14646" class="bi bi-x"
+                         viewBox="0 0 16 16">
+                      <path
+                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                  </span>
+              <img src="~/assets/img/logos/superjob.svg" />
+            </a>
+          </div>
         </div>
+      </div>
+      <div v-else>
+        No providers connected
+      </div>
+      </ClientOnly>
     </div>
-  <div v-else>
-      No providers connected
-  </div>
 </template>
 
 <script setup>
@@ -36,43 +54,35 @@ import {useResumeStore} from "~/store/resume";
 
 const vacancyStore = useVacancyStore();
 const resumeStore = useResumeStore();
-const { getProvidersAuthUrl } = useProfileStore();
+const { getSeekerProvidersAuthEndpoints } = useProfileStore();
 const { getConnectedProviders } = resumeStore;
 
 
+const providers = ref({
+  hh: {
+    slug: 'hh',
+    url: null,
+    is_connected: false,
+  },
+  superjob: {
+    slug: 'superjob',
+    url: null,
+    is_connected: false,
+  },
+});
 
+providers.value.hh.is_connected = resumeStore.providers.hh;
+providers.value.superjob.is_connected = resumeStore.providers.superjob;
 
-const providersList = ref([
-    {
-        name: 'HeadHunter',
-        slug: 'hh',
-        url: null,
-        is_connected: false,
-        icon: "https://tech.hh.ru/api/logos/min-hh-red.png",
-    },
-    {
-        name: 'Superjob',
-        slug: 'superjob',
-        url: null,
-        is_connected: false,
-        icon: new URL("~/assets/img/logos/superjob.svg", import.meta.url),
-    },
-]);
-
-const providers = computed(() => resumeStore.providers);
-console.log(resumeStore.providers);
-watch(() => providers.value, (newProviders) => {
+watch(() => resumeStore.providers, (newProviders) => {
     console.log(newProviders);
 
-    for (let i = 0; i < providersList.value.length; i++){
-        const providerItem = providersList.value[i];
-        if (newProviders[providerItem.slug])
-            providerItem.is_connected = newProviders[providerItem.slug];
-    }
+    providers.value.hh.is_connected = newProviders.hh;
+    providers.value.superjob.is_connected = newProviders.superjob;
 })
 
 const isAllConnected = computed(() => {
-    if (providers.value.hh && providers.value.superjob)
+    if (resumeStore.providers.hh && resumeStore.providers.superjob)
         return true;
     else return false;
 });
@@ -82,16 +92,9 @@ onMounted(async () => {
         getConnectedProviders();
     }
     if (!isAllConnected.value){
-        const authData = await getProvidersAuthUrl();
-        for (let i = 0; i < providersList.value.length; i++){
-            const providerItem = providersList.value[i];
-            if (authData[providerItem.slug]){
-              providerItem.url = authData[providerItem.slug];
-            }
-            if (resumeStore.providers[providerItem.slug]){
-              providerItem.is_connected = authData[providerItem.slug];
-            }
-        }
+        const authData = await getSeekerProvidersAuthEndpoints();
+        providers.value.hh.url = authData.hh;
+        providers.value.superjob.url = authData.superjob;
     }
 
 });
--- components/MyVacancies/ConnectedProviders.vue
+++ components/MyVacancies/ConnectedProviders.vue
@@ -1,27 +1,46 @@
 <template>
-    <div class="wrapper wrapper-1290" v-if="!isAllConnected">
+  <ClientOnly>
+    <div>
+      <div class="wrapper wrapper-1290" v-if="!isAllConnected">
         <h1 class="lk-page-title">Cервисы</h1>
         <div class="modal-content p-2 m-0 border-0" style="min-width: 10rem;">
-            <div class="list-of-providers">
-                <a @click="openProviderAuthUrl(item.url)" v-for="item in providers" class="provider-item">
+          <div class="list-of-providers">
+            <a @click="openProviderAuthUrl(providers.hh.url)"  class="provider-item">
                   <span class="provider-label success">
-                    <svg v-if="item.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
+                    <svg v-if="providers.hh.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
                          viewBox="0 0 16 16">
                       <path
-                              d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                          d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>
                     <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f14646" class="bi bi-x"
                          viewBox="0 0 16 16">
                       <path
-                              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                     </svg>
                   </span>
-                    <img :src="item.icon" />
-                </a>
-            </div>
+              <img :src="providers.hh.icon" />
+            </a>
+            <a @click="openProviderAuthUrl(providers.superjob.url)"  class="provider-item">
+                  <span class="provider-label success">
+                    <svg v-if="providers.superjob.is_connected" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="limegreen" class="bi bi-check"
+                         viewBox="0 0 16 16">
+                      <path
+                          d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f14646" class="bi bi-x"
+                         viewBox="0 0 16 16">
+                      <path
+                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                  </span>
+              <img :src="providers.superjob.icon" />
+            </a>
+          </div>
         </div>
+      </div>
+      <div v-else>No services</div>
     </div>
-  <div v-else></div>
+  </ClientOnly>
 </template>
 
 <script setup>
@@ -49,7 +68,7 @@ const providers = ref([
 ]);
 
 const vacancyStore = useVacancyStore();
-const { getProvidersAuthUrl,  } = useProfileStore();
+const { getEmployerProvidersAuthEndpoints,  } = useProfileStore();
 const { getConnectedProviders } = useVacancyStore();
 
 const checkProviders = async() => {
@@ -78,7 +97,7 @@ const isAllConnected = computed(() => {
 onMounted(async () => {
     await checkProviders();
     if (!isAllConnected.value){
-        const authData = await getProvidersAuthUrl();
+        const authData = await getEmployerProvidersAuthEndpoints();
         for (let i = 0; i < providers.value.length; i++){
             const providerItem = providers.value[i];
             providerItem.url = authData[providerItem.slug];
