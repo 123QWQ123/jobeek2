dfb5585b813813360a2998801c41d9d7738d4e82
Anpilogov <info@exsy.com.ua>
2024-03-04 15:42:03 +0200

    fix

--- components/MyResumes/ConnectedProviders.vue
+++ components/MyResumes/ConnectedProviders.vue
@@ -55,6 +55,9 @@
                 />
               </svg>
             </span>
+            <span class="provider-label reload">
+            	<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#5375FD" class="bi bi-arrow-repeat" viewBox="0 0 16 16" data-v-03b4701d=""><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" data-v-03b4701d=""></path><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z" data-v-03b4701d=""></path></svg>
+            </span>
             <img src="~/assets/img/logos/min-hh-red.png" class="w-100" />
           </a>
           <a
@@ -107,6 +110,9 @@
                   d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
                 />
               </svg>
+            </span>
+            <span class="provider-label reload">
+            	<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#5375FD" class="bi bi-arrow-repeat" viewBox="0 0 16 16" data-v-03b4701d=""><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" data-v-03b4701d=""></path><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z" data-v-03b4701d=""></path></svg>
             </span>
             <img src="~/assets/img/logos/superjob.svg" class="w-100" />
           </a>
