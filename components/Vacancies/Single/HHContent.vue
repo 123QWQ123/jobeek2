856aee9b262045e7f2666d1cef8c0c8bfdd27018
orzumurodparmonov <orzumurodparmonov@gmail.com>
2024-03-13 15:28:09 +0500

    favored add and remove done

--- components/Vacancies/Single/HHContent.vue
+++ components/Vacancies/Single/HHContent.vue
@@ -9,7 +9,7 @@
           <li v-for="phone in vacancyPhones">
             <a class="tel" :href="getPhoneHref(phone)">
               <img src="~/assets/img/svg/carbon_phone.svg" alt="#" />
-              +7 {{ phone }}
+              {{ phone }}
             </a>
           </li>
         </ul>
@@ -17,7 +17,14 @@
       </div>
       <div class="favorites-card-footer-row">
         <div class="group">
-          <button class="group-action btn button-md" @click="onSubmit">
+          <button
+            class="group-action btn button-md"
+            @click="onSubmit"
+            :class="{
+              'bg-primary': isSubmitButtonActive,
+              'text-white': isSubmitButtonActive,
+            }"
+          >
             Откликнуться
           </button>
           <button
@@ -56,6 +63,34 @@
           </button>
         </div>
       </div>
+
+      <div
+        class="favorites-card-footer-row telephones-row mt-2"
+        :style="{ display: isSubmitShown }"
+      >
+        <div v-if="my_resumes.length > 0">
+          <ul class="list-group" v-for="resume in my_resumes">
+            <li class="mt-1">
+              <button
+                class="btn btn-sm btn-outline-primary"
+                @click.prevent="submitResume(resume.id, item.id, item.provider)"
+              >
+                Откликатся
+              </button>
+              <span class="ms-2 h-3">
+                с
+                <NuxtLink
+                  class="ps-1"
+                  :to="{ name: 'my-resume-id', params: { id: resume.id } }"
+                >
+                  {{ resume.title }}
+                </NuxtLink>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <p v-else>Нет резюме</p>
+      </div>
     </div>
   </div>
   <div class="has-sidebar has-sidebar--v3">
@@ -71,15 +106,17 @@
             {{ item.education && item.education + "," }} {{ item.work_type }},
             {{ moment.unix(item.published_date).format("YYYY.MM.DD") }}
           </div>
-          <div class="salary" v-if="item.salary_to && item.salary_from">
-            {{ salary_from }} — {{ salary_to }} {{ item.currency }}/месяц
-          </div>
-          <div class="salary" v-else-if="item.salary_from">
-            от {{ salary_from }} {{ item.currency }}/месяц
-          </div>
-          <div class="salary" v-else-if="item.salary_to">
-            {{ salary_to }} {{ item.currency }}./месяц
-          </div>
+          <client-only>
+            <div class="salary" v-if="item.salary_to && item.salary_from">
+              {{ salary_from }} — {{ salary_to }} {{ item.currency }}/месяц
+            </div>
+            <div class="salary" v-else-if="item.salary_from">
+              от {{ salary_from }} {{ item.currency }}/месяц
+            </div>
+            <div class="salary" v-else-if="item.salary_to">
+              {{ salary_to }} {{ item.currency }}./месяц
+            </div>
+          </client-only>
         </div>
         <div class="vacancy-single-body" v-html="item.description"></div>
         <div class="vacancy-single-footer">
@@ -106,19 +143,27 @@ import moment from "moment";
 import { useVacancyStore } from "~/store/vacancy";
 import Swal from "sweetalert2";
 import { useResumeStore } from "~/store/resume.js";
+import { useSSRContext } from "vue";
+import { toast } from "vue3-toastify";
 
 const { item } = defineProps({
   item: {
     required: true,
   },
 });
-
+// Computed property to determine rendering environment
+const isServer = computed(() => {
+  // useSSRContext returns the SSR context on the server or undefined on the client
+  return !!useSSRContext();
+});
+const { $format_number } = useNuxtApp();
+console.log(item);
 const isFavorite = ref(item.is_favorite ?? false);
 const salary_from = computed(() => {
-  return vueNumberFormat(item.salary_from, {});
+  return $format_number(item.salary_from);
 });
 const salary_to = computed(() => {
-  return vueNumberFormat(item.salary_to, {});
+  return $format_number(item.salary_to);
 });
 
 const vacancyStore = useVacancyStore();
@@ -128,7 +173,7 @@ const toggleFavorite = async () => {
   if (!isFavorite.value === true) {
     response = await addToFavorite({ id: item.id, provider: "hh" });
   } else {
-    response = await removeFromFavorite({ id: item.id, provider: "hh" });
+    response = await removeFromFavorite(item.id, { provider: "hh" });
   }
   if (response.status === "success") {
     isFavorite.value = !isFavorite.value;
@@ -145,14 +190,35 @@ const toggleFavorite = async () => {
 const resumeStore = useResumeStore();
 
 const { getMyResumes } = resumeStore;
+const { my_resumes } = storeToRefs(resumeStore);
+const isSubmitShown = ref("none");
 
+const submitResume = async (resume_id, vacancy_id, provider) => {
+  console.log(resume_id, vacancy_id, provider);
+};
+const isSubmitButtonActive = ref(false);
 const onSubmit = async (e) => {
+  if (isSubmitButtonActive.value) {
+    isSubmitButtonActive.value = false;
+    isSubmitShown.value = "none";
+    return;
+  }
   e.preventDefault();
   if (resumeStore.my_resumes.length < 1) {
     await getMyResumes();
+  }
+  if (resumeStore.my_resumes.length < 1) {
+    toast.info("Нет резюму чтобы откликатся.");
     return;
   }
   console.log(resumeStore.my_resumes);
+  if (resumeStore.my_resumes.length > 1) {
+    isSubmitButtonActive.value = true;
+    isSubmitShown.value = "block";
+    return;
+  }
+
+  console.log(1);
   let response = {};
   // response = await removeFromFavorite({ id: item.id, provider: "hh" });
   // if (response.status === "success") {
