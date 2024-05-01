b0f4904d5b93f37158f78d5e0313a21b11d0e3a9
orzumurodparmonov <orzumurodparmonov@gmail.com>
2024-05-01 13:18:18 +0500

    login and register page zoom in and out issue autosize fixed

---
+++ components/Hv.vue
@@ -0,0 +1,28 @@
+<script setup>
+watch(
+  () => window.innerHeight,
+  () => {},
+);
+
+onMounted(() => {
+  const updateVH = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+  };
+  window.addEventListener("resize", updateVH);
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+});
+
+onBeforeUnmount(() => {
+  const updateVH = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+  };
+  window.removeEventListener("resize", updateVH);
+});
+</script>
+
+<template></template>
+
+<style scoped></style>
