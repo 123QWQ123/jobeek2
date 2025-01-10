e973300f793dc843b4a960f070b6dba37e3ac9bf
a.novickiy <zzz>
2025-01-10 19:56:01 +0100

    Remove server-side check in VueNumberFormat plugin initialization
    The server-side check was unnecessary as the plugin can safely be initialized on both client and server. This change simplifies the code and ensures consistent behavior regardless of the environment.

--- plugins/vueNumberFormat.js
+++ plugins/vueNumberFormat.js
@@ -1,12 +1,10 @@
 import VueNumberFormat from "vue-number-format";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (!process.server) {
-    nuxtApp.vueApp.use(VueNumberFormat, {
-      prefix: "",
-      decimal: "",
-      thousand: " ",
-      precision: 0,
-    });
-  }
+  nuxtApp.vueApp.use(VueNumberFormat, {
+    prefix: "",
+    decimal: "",
+    thousand: " ",
+    precision: 0,
+  });
 });
